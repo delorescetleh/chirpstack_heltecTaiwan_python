54cc392cf91f6784d62292c3c5fc52ee7418e78d
delores.cetleh <delores.cetleh@gmail.com>
2021-06-19 20:00:40 +0800

    add live.py

---
+++ live.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+import json
+
+import paho.mqtt.client as mqtt
+import requests
+
+lives = {}
+
+# 當地端程式連線伺服器得到回應時，要做的動作
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # 將訂閱主題寫在on_connet中
+    # 如果我們失去連線或重新連線時
+    # 地端程式將會重新訂閱
+    client.subscribe("application/+/device/+/event/+")
+
+# 當接收到從伺服器發送的訊息時要進行的動作
+
+
+def on_message(client, userdata, msg):
+    deviceInfo = msg.payload.decode('UTF-8')
+    sendData = json.dumps({'deviceInfo': deviceInfo})
+    deviceInfo = {'deviceInfo': deviceInfo}
+    print(deviceInfo)
+    try:
+        r = requests.post("http://127.0.0.1:8083/mqttComing",
+                          data=deviceInfo, timeout=3)
+    except:
+        print("An exception occurred")
+
+
+# 連線設定
+# 初始化地端程式
+client = mqtt.Client()
+
+# 設定連線的動作
+client.on_connect = on_connect
+
+# 設定接收訊息的動作
+client.on_message = on_message
+
+# 設定登入帳號密碼
+client.username_pw_set("chirpstack_as_events", "chirpstack_as_events_password")
+
+# 設定連線資訊(IP, Port, 連線時間)
+client.connect("127.0.0.1", 1883, 60)
+
+# 開始連線，執行設定的動作和處理重新連線問題
+# 也可以手動使用其他loop函式來進行連接
+client.loop_forever()
+
